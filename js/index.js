e25f4aef487979625c852bb660b3917345b61ecb
MAPLELEAF3659 <ig910425@gmail.com>
2022-03-22 18:20:24 +0800

    add currency converter

--- js/index.js
+++ js/index.js
@@ -16,3 +16,36 @@ Vue.createApp({
         }
     }
 }).mount("#app")
+Vue.createApp({
+    data() {
+        return {
+            twd: 1
+        }
+    },
+    // methods: {
+    //     twd2jpy: function() {
+    //         this.jpy = this.twd * 4.2663;
+    //     },
+    //     jpy2twd: function() {
+    //         this.twd = this.jpy / 4.2663;
+    //     }
+    // }
+    computed: {
+        jpy: {
+            get() {
+                return this.twd * 4.2263;
+            },
+            set(val) {
+                this.twd = val / 4.2263;
+            }
+        },
+        usd: {
+            get() {
+                return this.twd / 28.45;
+            },
+            set(val) {
+                this.twd = val * 28.45;
+            }
+        }
+    }
+}).mount("#converter")
